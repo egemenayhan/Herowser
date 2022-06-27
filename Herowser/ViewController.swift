8b1f64ece7aae851170999a4bd3385391eef6e3a
Egemen Ayhan <egemenayhan@icloud.com>
2022-06-27 21:45:00 +0300

    Initialize project

---
+++ Herowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Herowser
+//
+//  Created by Egemen Ayhan on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Herowser-HerowserUITests/Pods-Herowser-HerowserUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Herowser_HerowserUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Herowser_HerowserUITests
+@end
